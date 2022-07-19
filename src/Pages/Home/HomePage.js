23dd286c5171fd864d4977a11986b5907a057c71
Igor de Castro Tristão <igorcastro55@hotmail.com>
2022-07-19 12:06:47 -0300

    igor-map

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,25 +1,51 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { goToDetails, goToPokedex } from '../../Routes/coordinator'
-import { Container } from './homePageStyled'
-import CardHomePage from './CardHomePage'
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { goToDetails, goToPokedex } from "../../Routes/coordinator";
+import { Container } from "./homePageStyled";
+import CardHomePage from "./CardHomePage";
+import axios from "axios";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const [infoPokemons, setInfoPokemons] = useState();
 
-        const navigate = useNavigate()
-    
-        
+  useEffect(() => {
+    listPokemons();
+  }, []);
+
+  const listPokemons = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`).then((res) => {
+      const pokemonsDetails = res.data.results.map(({ url }) =>
+        axios.get(url).then((res) => {
+          return res.data;
+        })
+      );
+      Promise.all(pokemonsDetails).then((res) => setInfoPokemons(res));
+    });
+  };
+
+  console.log(infoPokemons)
+
+  const renderPokemons = infoPokemons?.map((pokemon) => {
+    return (
+      <div>
+        <p>{pokemon.id}. {pokemon.name}</p>
+        <p>{pokemon.types[0].type.name}</p>
+        <p>{pokemon.types[1]?.type.name}</p>
+      </div>
+    )
+  })
+
   return (
-
     <Container>
       <h1>Todos os Pokémons</h1>
       {/* <CardHomePage/> */}
-    <button onClick={() => goToDetails(navigate)}>DETAILS</button>
-    <button onClick={() => goToPokedex(navigate)}>Pokedex</button>
-
+      {renderPokemons}
+      <p>dsadsadsadsada</p>
+      <button onClick={() => goToDetails(navigate)}>DETAILS</button>
+      <button onClick={() => goToPokedex(navigate)}>Pokedex</button>
     </Container>
-  )
-  }
+  );
+};
 
-export default HomePage
+export default HomePage;
